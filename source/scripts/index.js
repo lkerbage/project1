173b196df4b40fa7f50969d3f3d0058ab28cceac
kerbage <lkerbage@gmx.net>
2022-06-17 16:45:41 +0200

    delete-fn, added todo for this app

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,19 +1,7 @@
-window.addEventListener('load', () => {
-  const deleteButtons = document.querySelectorAll('.deleteButton');
-
-  deleteButtons.forEach((button) => {
-    button.addEventListener('click', (ev) => {
-  		console.log(ev.target);
-    });
-  });
-
-  console.log(deleteButtons[0].dataset.id);
-  console.log(deleteButtons);
-});
-
 const create = document.querySelector('#create');
 const form = document.querySelector('#form');
 const ul = document.querySelector('ul');
+const todoTitle = document.querySelector('.todo-title');
 const data = JSON.parse(localStorage.getItem('todos')) || [];
 const dataLength = data.length;
 
@@ -28,19 +16,18 @@ const getData = () => {
     .map(
       (todo, index) => `<li class="container wrapper" data-id="${index}">${todo.title} und ${todo.description}<button class="deleteButton" data-id="${index}">Löschen</button><button class="editButton"  data-id="${index}">Bearbeiten</button></li>`,
     )
-    .join('');
+    .join(''); console.log(data);
 };
 const getNumberOfTodos = () => {
-  // completed / incompleted
+  todoTitle.innerHTML = `<h2>${dataLength === 1 ? `${dataLength} Todo` : `${dataLength} Todo's`}</h2>`;
 };
 
 function init() {
   if (dataLength > 0) {
     getData();
-    getNumberOfTodos();
+    getNumberOfTodos(dataLength);
   } else ul.innerHTML = '<h2>Du bist frei 😀 Es gibt heute nichts zu machen!</h2>';
 }
-init();
 
 const submitTodo = () => {
   const newTodo = {
@@ -53,7 +40,6 @@ const submitTodo = () => {
   };
   data.push(newTodo);
   localStorage.setItem('todos', JSON.stringify(data));
-
   getData();
 };
 
@@ -69,8 +55,11 @@ const validationText = () => {
   }
 };
 
-const deleteItem = (ev) => {
-  alert(ev);
+const deleteItem = (id) => {
+  console.log('delete item', id);
+  const filtered = data.filter((item) => item.id !== 0);
+  localStorage.setItem('todos', JSON.stringify(filtered));
+  getData();
 };
 
 create.addEventListener('click', () => console.log('clicked create button'));
@@ -78,3 +67,21 @@ form.addEventListener('submit', (e) => {
   submitCreate(e);
 });
 title.addEventListener('input', validationText);
+
+window.addEventListener('load', () => {
+  const deleteButtons = document.querySelectorAll('.deleteButton');
+  const editButtons = document.querySelectorAll('.editButton');
+  console.log('deleteButtons', deleteButtons);
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener('click', (ev) => {
+      const { id } = ev.target.dataset;
+      deleteItem(id);
+    });
+  });
+
+  editButtons.forEach((button) => {
+    button.addEventListener('click', (ev) => ev.target.dataset.id);
+  });
+  init();
+});
