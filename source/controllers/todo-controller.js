b0a027122c2d9a846cae63f39aed624aebdcb8ba
kerbage <lkerbage@gmx.net>
2022-06-25 07:50:14 +0200

    refactoring

--- source/controllers/todo-controller.js
+++ source/controllers/todo-controller.js
@@ -16,10 +16,7 @@ class TodoController {
 
     this.addNote = async (req, res) => {
       try {
-        console.log(req.body);
         res.json(await store.add(req.body));
-        // eslint-disable-next-line max-len
-        await store.add(req.body).then((result) => { res.send(result); }).catch((err) => { res.send(err); });
         res.send(req.body);
       } catch {
         res.status(500).json('DB failure when retrieving notes (POST))');
@@ -31,23 +28,6 @@ class TodoController {
         res.json(await store.update(req.body.note));
       } catch (err) {
         res.status(500).json('DB failure when retrieving notes (update))');
-      }
-    };
-
-    this.getNoteById = async (req, res) => {
-      try {
-        res.json(await store.get(req.params.id));
-      } catch (err) {
-        res.status(500).json('DB failure when retrieving note');
-      }
-    };
-
-    this.deleteNote = async (req, res) => {
-      try {
-        const result = await store.delete(req.params.id);
-        if (result.numDeleted === 0) { res.status(404).json('Note not found'); } else { res.status(202).json(result.revision); } // accepted, but note is just marked as deleted in db-file.
-      } catch (err) {
-        res.status(500).json('DB failure when deleting note');
       }
     };
   }
