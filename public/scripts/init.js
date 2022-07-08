549850d1b8dc3f4c18db04cee0b9c5893d81f5b4
kerbage <lkerbage@gmx.net>
2022-07-08 17:26:06 +0200

    refactoring

--- public/scripts/init.js
+++ public/scripts/init.js
@@ -5,21 +5,23 @@ import {
   selectorDisplayBottom,
   selectorDisplayTop,
   showByStatus,
-  submitButton,
   todoFilter,
   todoFilters,
   todoList,
   toggle,
   ul,
 } from '../constants/elements.js';
-import { Display } from '../constants/enums.js';
-import { setTheme } from './theme.js';
-import { validationText } from './validationText.js';
-import { processItem } from '../services/process-item.js';
-import { completedItems, filterItems } from './filter.js';
-import { displayParts } from './displayParts.js';
-import { displayData, fetchData, titleFocus } from './index.js';
-import { submit } from './submit.js';
+import {Display} from '../constants/enums.js';
+import {setTheme} from './theme.js';
+import {validationText} from './validationText.js';
+import {processItem} from '../services/process-item.js';
+import {completedItems, filterItems} from './filter.js';
+import {displayParts} from './displayParts.js';
+import {displayData, fetchData} from './index.js';
+import {submit} from './submit.js';
+import {createNewTodo} from "./createNewTodo.js";
+import {overview as overviewFunction} from "./overview.js";
+
 
 export async function init() {
   displayParts(selectorDisplayTop, Display.NONE);
@@ -43,16 +45,10 @@ export async function init() {
   todoFilter.addEventListener('change', (ev) => filterItems(ev, data));
   showByStatus.addEventListener('change', (ev) => completedItems(ev, data));
   createButton.addEventListener('click', () => {
-    displayParts(selectorDisplayBottom, Display.NONE);
-    displayParts(selectorDisplayTop, Display.BLOCK);
-    titleFocus();
-    form.reset();
-    submitButton.innerHTML = 'Erstellen';
-    submitButton.classList.add('js-create-new-todo');
+   createNewTodo()
   });
   overview.addEventListener('click', () => {
-    displayParts(selectorDisplayTop, Display.NONE);
-    displayParts(selectorDisplayBottom, Display.BLOCK);
+    overviewFunction()
   });
   toggle.addEventListener('click', () => setTheme());
 }
